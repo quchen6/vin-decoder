237fce3c674c99088030b133696925828a16a496
jorgevaldivia <jorge+dev@valdivia.me>
2013-09-30 20:10:59 -0500

    switch from fakeweb to webmock for tests

--- Gemfile
+++ Gemfile
@@ -48,7 +48,7 @@ group :development, :test do
   gem 'rspec-rails', '~> 2.0'
 	gem 'factory_girl_rails', '4.2.1'
 	gem "vcr"
-	gem "fakeweb"
+	gem "webmock"
 end
 
 gem "rest-client"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,16 @@ require 'rspec/rails'
 require 'rspec/autorun'
 require 'factory_girl_rails'
 
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+end
+
+
 VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join "spec", "vcr_cassettes"
-  c.hook_into :fakeweb
+  c.hook_into :webmock
 end
