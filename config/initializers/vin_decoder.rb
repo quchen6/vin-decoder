e6c9b1ab5106d5f57b1f90aa0d9bf90c90c0dc10
jorgevaldivia <jorge+dev@valdivia.me>
2013-09-30 20:12:14 -0500

    added vin decoder library

---
+++ config/initializers/vin_decoder.rb
@@ -0,0 +1,22 @@
+class VinDecoder
+
+	BASE_URL = "https://api.edmunds.com/v1/api/toolsrepository/vindecoder"
+
+	attr_accessor :vin, :data
+
+	def initialize vin
+		@vin 				= vin
+		@data 			= decode
+	end
+
+	protected
+
+	def decode
+		res 				= RestClient.get "#{BASE_URL}?vin=#{@vin}&fmt=json&api_key=#{EDMUNDS_API_KEY}"
+		parsed_res 	= JSON.parse(res.body)
+
+		# Return only the relevant data
+		parsed_res["styleHolder"].try :first
+	end
+
+end
